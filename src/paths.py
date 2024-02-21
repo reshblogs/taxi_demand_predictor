14b21fc8f2f6acb70a61a4081240fcf2b603abdb
Resh <reshblogshere@gmail.com>
2024-02-21 18:54:18 +0100

    Directory creation commands changed

--- src/paths.py
+++ src/paths.py
@@ -10,14 +10,14 @@ MODEL_PATH = project_path + '/model'
 ENV_PATH = project_path
 
 if not os.path.exists(RAW_PATH):
-    os.mkdir(RAW_PATH)
+    os.makedirs(RAW_PATH,exist_ok=True) #Creates necessary parent directories also
     
 if not os.path.exists(VALIDATED_PATH):
-    os.mkdir(VALIDATED_PATH)
+    os.makedirs(VALIDATED_PATH,exist_ok=True)
     
 if not os.path.exists(TRANSFORMED_PATH):
-    os.mkdir(TRANSFORMED_PATH)
+    os.makedirs(TRANSFORMED_PATH,exist_ok=True)
 
 if not os.path.exists(MODEL_PATH):
-    os.mkdir(MODEL_PATH)
+    os.makedirs(MODEL_PATH,exist_ok=True)
     
